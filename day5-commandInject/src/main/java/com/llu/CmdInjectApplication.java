a925b9e9bebb85409b1ce5ce1adb237b5d3d3178
浩仔 <liuhaojie0225@163.com>
2024-07-16 18:03:11 +0800

    day5-命令执行漏洞

---
+++ day5-commandInject/src/main/java/com/llu/CmdInjectApplication.java
@@ -0,0 +1,11 @@
+package com.llu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmdInjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CmdInjectApplication.class, args);
+    }
+}
